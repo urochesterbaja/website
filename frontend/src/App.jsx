cf8b914e867f76be99f3baec708354af4ad6bcbe
Will Hager <111020616+willhager@users.noreply.github.com>
2025-12-18 20:42:03 -0500

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Nav"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1 className="logo">Yellowjacket Racing</h1>
+            <ul className="nav-links">
+                <li href="/">Home</li>
+                <li href="/news">News</li>
+                <li href="/results">Results</li>
+                <li href="/sponsorship">Sponsorship</li>
+                <li href="/contact">Contact Us</li>
+            </ul>
+        </nav>
+        
+    );
+}
+export default Navbar;
