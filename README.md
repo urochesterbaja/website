0132bace5187a5da45eb605ac4ab6d1c5d34b514
Will Hager <111020616+willhager@users.noreply.github.com>
2025-12-18 23:44:21 -0500

    init commit 2

--- README.md
+++
@@ -1,8 +0,0 @@
-to build:
-brew install node
-npm install -g netlify-cli
-git clone https://github.com/urochesterbaja/website
-cd <project-folder>
-npm install
-cd frontend
-npm install
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Nav"
+import './App.css'
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
+
+export default App;
