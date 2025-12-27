1b1c6673b60860ebc7a2537edf79d67c171d58ee
Will Hager <111020616+willhager@users.noreply.github.com>
2025-12-27 11:08:16 -0500

    hero modularity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import Sponsorship from "./pages/Sponsorship"
 import Navbar from "./components/Nav"
 import Hero from "./components/Hero"
+import Footer from "./components/Footer"
 import { HeroPropsMap } from "./components/HeroPropsMap"
 import './App.css'
 
+/* Layout function is here so whole function can be wrapped in BrowserRouter.
+*  This is so the Hero can access the location of the site, allowing for modular component usage*/
 function Layout() {
   const location = useLocation();
   let pageKey = location.pathname;
@@ -14,12 +18,14 @@ function Layout() {
       <Hero {...HeroPropsMap[pageKey]}/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/sponsorship" element={<Sponsorship/>} />
       </Routes>
-      
+      <Footer />
     </>
   );
 }
 
+/* This is the actual "App" that gets returned to main.jsx and index.html */
 function App() {
   return(
     <BrowserRouter>
--- frontend/src/components/HeroPropsMap.js
+++ frontend/src/components/HeroPropsMap.js
@@ -3,5 +3,10 @@ export const HeroPropsMap = {
         title: "YELLOWJACKET RACING",
         subtitle: "University of Rochester's Premier Baja SAE Team",
         bgImgURL: "/banner_image_temp.png"
+    },
+    "/sponsorship": {
+        title: "SPONSORSHIP",
+        subtitle: "",
+        bgImgURL: "/banner_image_temp.png"
     }
 }
