70b9ce10f4a95908e7ea9bc50231a57fa83ff0e9
Will Hager <111020616+willhager@users.noreply.github.com>
2026-02-24 23:59:35 -0500

    css fixes

--- frontend/src/components/PartnerBlock/PartnerBlock.jsx
+++ frontend/src/components/PartnerBlock/PartnerBlock.jsx
@@ -3,7 +3,7 @@ import "./PartnerBlock.css"
 function PartnerBlock ({tier, imgList = []}) {
     return (
         <div className="partner-block">
-            <h2 className="partner-heading"><i>{tier} SPONSORS</i></h2>
+            <h2 className="partner-heading"><i>{tier}</i></h2>
             <div className="img-container">
                 {imgList.map((item, index) => {
                     const imageElement = item.img ? <img src={item.img}></img> : <h2 className="logo-replacement">{item.name}</h2>
